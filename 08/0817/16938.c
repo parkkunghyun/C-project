25d36be9c8fb3f184fd6f6af31192c90b929ff30
baggyeonghyeon <rudgus4620@gmail.com>
2023-08-17 13:14:45 +0900

    sdfd

---
+++ 08/0817/16938.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+// 문제를 N개 가지고 있고 난이도는 정수 배열
+// 두문제이상, 합은L과 R사이 둘 포함, 차는 X만큼 차이
+// 브루트포스? 다 확인해야하나?
+// 백트래킹 -> x가 안맞으면 나가리! L R사이 아니면 나가리!
+// 숫자가 같아도 배열 인덱스가 다르면 다르게 보네!
+// 일단 모든 조합을 구하고 그거를 계산해서 맞으면 count+1 아니면 pass
+
+int N,L,R,X;
+int arr[16];
+int count = 0;
+int visited[16];
+
+void clear()
+{
+    for (int i =0; i< N; i++)
+    {
+        visited[i] = 0;
+    }
+}
+
+void combination(int len, int depth, int start, int sum, int minus)
+{
+    // 여기서 만들어지는 즉시 체크하는 함수로 보내버리자!
+    if (depth == len)
+    {
+        if (R <= sum && sum <= L && sum >= X)
+            count += 1;
+        return ;
+    }
+
+    for (int i = start; i< N; i++)
+    {
+        if (visited[i] == 0)
+        {
+            visited[i] = 1;
+            sum += arr[i];
+            if (minus == -1)
+                minus = arr[i];
+            else
+            {
+                if (minus < arr[i])
+                    minus = arr[i] - minus;
+                else
+                    minus -= arr[i];
+            }
+            combination(len, depth + 1, i, sum, minus);
+            visited[i] = 0;
+        }
+    }
+}
+
+int main(void)
+{
+    scanf("%d %d %d %d", &N,&L, &R, &X);
+    for(int i =0; i< N; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for(int i = 2; i<=N; i++)
+    {
+        clear();
+        combination(i, 0, 0, 0 ,-1); // 일단 조합을 만들고 그 조합들을 계산하기
+    }
+
+    printf("%d\n", count);
+    return (0);
+}
