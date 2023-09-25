a0a209fa7a69b1e6870021758d431a41f3dccfd9
baggyeonghyeon <rudgus4620@gmail.com>
2023-09-25 09:55:47 +0900

    safd

--- b2290.java
+++ b2290.java
@@ -30,7 +30,7 @@ public class b2290 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int S = Integer.parseInt(st.nextToken(br.readLine()));
+        //int S = Integer.parseInt(st.nextToken(br.readLine()));
         String N = st.nextToken(br.readLine());
 
         for (int i = 0; i< N.length(); i++) {
---
+++ b25542.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+/*
+길이가 L 대문자
+N개 가게 중에 한글자만 다르고 이길이가 L인 가게 
+CALL FRIEND
+각각이 한글자만 달라야함
+N개 중에 1개 빼고 다 같으면 됨 
+알파벳 배열을 놔둔뒤 맞으면 그 단어 놔두기!
+
+ */
+
+public class b25542 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        
+    }
+}
