120d1b64ab261a7de2a66498f74c3eed2d5d8140
baggyeonghyeon <rudgus4620@gmail.com>
2023-12-23 14:56:09 +0900

    sdfa

--- b11497.java
+++
@@ -1,6 +0,0 @@
-public class b11497 {
-    public static void main(String[] args) {
-        
-    }
-    
-}
---
+++ b2506.java
@@ -0,0 +1,5 @@
+public class b2506 {
+    public static void main(String[] args) {
+        System.out.println("helloo");
+    }
+}
