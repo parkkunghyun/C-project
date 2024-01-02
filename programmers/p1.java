d1e761ab1251396ca222dd6ed6897114205bbed6
baggyeonghyeon <rudgus4620@gmail.com>
2024-01-02 14:52:03 +0900

    Asdf

--- programmers/p1.java
+++ programmers/p1.java
@@ -1,29 +1,15 @@
 package programmers;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 /*
  모든 직선쌍에 대한 반복을 통해 -> 정수인 교점 좌표만 구해야함
  저장된 정수들 중에 x,y좌표 최고 최저를 각각 찾기
  최솟값 최댓값을 통해 2차원 배열 작성하기
  2차원 배열에 별 표시
 
-
-1,4 / -4,4 / -4,-4 / 1, -4 / 4. 0
-
-0,4 
-3,0 3,8
-8,0 8,8
-
-
 maxy = 4 -
 maxX = 4
 minY = -4
 minX = -4 - 
-
-
 
  해결법
   좌표 문제가 나왔을때 Point라는 클래스를 만들어주자
@@ -34,87 +20,71 @@ minX = -4 -
 double에서 정수일때만을 찾는다면 1로 나눴을때 나머지가 0이어야한다! -> 소수점이 있으면 아래 소수가 너머지로 나옴
  */
 
+ /*
+  삼각달팽이
+
+  n n 2차원 배열로 선언
+  숫자를 채울 현재 위치를 0,0으로 설정
+  방향에 따라 이동 못할때 까지 숫자 채우기
+    - 아래로 이동하면서 숫자
+    - 오른쪽으로 이동하면서 숫자
+    - 왼쪽 위로 이동하면서 숫자
+  다시 1차원에 넣기
+  */
+
+  // 각 더한것만큼 개수를 구하려면 반복문을 쓰지 말고 (n - 1) * n / 2를 사용하자!
+  // 
+
 public class p1 {
-     public static void main(String[] args) {
-        System.out.println("hello");
-    }
-
-    private static class Point {
-        public final long x, y;
-        private Point(long x, long y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-    private static Point intersection(long a1, long b1, long c1, long a2, long b2, long c2) {
-        double x = (double)( (b1 * c2 - c1 * b2) / (a1 * b2 - b1 * a2));
-        double y = (double)( (c1 * a2 - a1 * c2) / (a1 * b2 - b1 * a2));
-        if (x % 1 != 0 || y % 1 != 0) return null;
-        return new Point((long)x, (long)y);
-    }
-
-    private static Point getMinimumPoint(List<Point> points) {
-        long x = Long.MAX_VALUE;
-        long y = Long.MAX_VALUE;
-
-        for (Point p : points) {
-            if (p.x < x) x = p.x;
-            if (p.y < y) y = p.x;
-        }
-        // 각각에서 가장 작은 x y를 구해서 포인트로 반환시킴!
-        return new Point(x, y);
-    }
-
-    private static Point getMaximumPoint(List<Point> points) {
-         long x = Long.MIN_VALUE;
-         long y = Long.MIN_VALUE;
-
-        for (Point p : points) {
-            if (p.x > x) x = p.x;
-            if (p.y > y) y = p.x;
-        }
-        // 각각에서 가장 작은 x y를 구해서 포인트로 반환시킴!
-        return new Point(x, y);
-    }
-
-
-    public static String[] solution(int[][] line) {
-        String [] answer = {};
-        List<Point> points = new ArrayList<>();
-
-        // 모든 직선 쌍에 대한 반복
-        for (int i = 0; i < line.length; i++) {
-            for (int j = i + 1; j < line.length; j++) {
-                Point intersection = intersection(line[i][0], line[i][1], line[i][2], line[j][1], line[j][1], line[j][2]);
-                if (intersection != null) {
-                    points.add(intersection);
+    public static int[][] solution(int[][] arr1, int[][] arr2) {
+        
+        int[][] answer = new int[arr1.length][arr2[0].length];
+        //System.out.println(arr1.length * arr2[0].length);
+        System.out.println(arr2[1][0]);
+        arr2[1][0] = 3;
+        int aIdx = 0;
+        int aIdx2 = 0;
+        
+        for (int i = 0; i < arr1.length; i++) {
+            for (int k = 0; k < arr2[0].length; k++) {
+                int temp = 0;
+                for (int j = 0; j < arr2.length; j++) {
+                   temp += arr1[i][j] * arr2[j][k];
                 }
+                answer[aIdx][aIdx2] = temp;
+                aIdx2++;
             }
+            aIdx2 = 0;
+            aIdx++;
         }
+        return answer;
+    }
 
-        Point minimum = getMinimumPoint(points);
-        Point maximum = getMaximumPoint(points);
+     public static void main(String[] args) {
+        int [][] arr1 = new int[3][2];
+        int [][] arr2 = new int[2][2];
 
-        int width = (int)(maximum.x - minimum.x + 1);
-        int height= (int)(maximum.y - minimum.y + 1);
+        arr1[0][0] = 1;
+        arr1[0][1] = 4;
+        arr1[1][0] = 3;
+        arr1[1][1] = 2;
+        arr1[2][0] = 4;
+        arr1[2][1] = 1;
 
-        char [][] arr = new char[height][width];
-        for (char [] row : arr) {
-            Arrays.fill(row, '.');
-        }
+        arr2[0][0] = 3;
+        arr2[0][1] = 3;
+        arr2[0][0] = 3;
+        arr2[1][1] = 3;
 
-        for (Point p : points) {
-            int x =  (int)(p.x - minimum.x);
-            int y = (int)(maximum.y - p.y);
-            arr[y][x] = '*';
-        }
+       //solution(arr1, arr2);
+        char a = 'a';
+        System.out.println(a == 'a');
+       String p = "p00p";
+       char [] arr3 = new char[4];
+       for (int i = 0; i < 4; i++) {
+        arr3[i] = p.charAt(i);
+       }
+       System.out.println(arr3);
 
-        String [] result = new String[height];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = new String(arr[i]);
-        }
-
-        return result;
     }
 }
